dcba36ea1c0e5cf75a158ee4925aaa5f02b427e3
Joe Kowalczyk <josephdkowalczyk61@gmail.com>
2021-10-01 18:45:03 -0500

    got the charset to confirm

--- Develop/script.js
+++ Develop/script.js
@@ -4,8 +4,6 @@ const numbers = "1234567890";
 const specChar = "!@#$%^&*()-+_="; 
 
 //figure length 
-
-
 function generatePassword(){
   var passLength 
   while (!Number.isInteger(parseInt(passLength))) {
@@ -13,23 +11,34 @@ function generatePassword(){
 }{ while (passLength <8 || passLength >128)
   passLength = prompt("how long")
 }{ if (passLength > 8 && passLength < 128)
-  console.log(passLength)
-}
-    
-
-
-  var lowerCaseConfirm = confirm("do you lc?");
-  console.log(lowerCaseConfirm)
-  var upperCaseConfirm = confirm("do you uc?");
-  console.log(upperCaseConfirm)
-  var specialCaseConfirm = confirm("do you sp?");
-  console.log(specialCaseConfirm)
-  var numConfirm = confirm("do you num?");
-  console.log(numConfirm)
-  
-
+  length = passLength
 }
 
+var charSet = ""
+
+var lowerCaseConfirm = confirm ("Would you like lower case letters?");
+  if (lowerCaseConfirm){
+  charSet += lowerCase; 
+  }
+var upperCaseConfirm = confirm ("would you like upper case letters?");
+  if (upperCaseConfirm){
+  charSet += upperCase;
+  }
+var numConfirm = confirm ("would you like numbers?");
+  if (numConfirm){
+  charSet += numbers;
+  }
+var specialCaseConfirm = confirm ("would you like to add a special character?");
+  if (specialCaseConfirm){
+  charSet += specChar;
+  }
+  var yourPassword = "";
+//for loop that takes the password length then adds from the top strings to randomize and output a password into "your password"
+for (var i = 0; i < passLength; i++){
+  yourPassword += charSet[Math.floor(Math.random() * charSet.length)];
+}
+return yourPassword;   
+}
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
